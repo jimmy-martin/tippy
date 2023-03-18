0fb9f178637d4772c424abb4e14362e7cf45989d
jimmy-martin <jimmy.martin952@gmail.com>
2023-03-18 17:47:27 +0100

    feat: Created ADMIN CRUD and created exclude function to remove key(s) from array

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,9 +1,34 @@
 const prisma = require('../config/prismaClient');
+const { exclude } = require('../utils/exclude');
 
 module.exports = {
-  findMany: () => prisma.admin.findMany(),
-  findOne: (id) => prisma.admin.findOne({ where: { id } }),
-  create: (data) => prisma.admin.create({ data }),
-  update: (id, data) => prisma.admin.update({ where: { id }, data }),
-  delete: (id) => prisma.admin.delete({ where: { id } }),
+  findMany: async () => {
+    const admins = await prisma.admin.findMany();
+    return admins.map((admin) => exclude(admin, ['pincode']));
+  },
+
+  findOne: async (id) => {
+    const admin = await prisma.admin.findUniqueOrThrow({
+      where: { id: Number(id) },
+    });
+    return exclude(admin, ['pincode']);
+  },
+
+  create: async (data) => {
+    const admin = await prisma.admin.create({ data });
+    return exclude(admin, ['pincode']);
+  },
+
+  update: async (id, data) => {
+    const admin = await prisma.admin.update({
+      where: { id: Number(id) },
+      data,
+    });
+    return exclude(admin, ['pincode']);
+  },
+
+  delete: async (id) => {
+    const admin = await prisma.admin.delete({ where: { id: Number(id) } });
+    return exclude(admin, ['pincode']);
+  },
 };
--- src/services/AdminService.js
+++ src/services/AdminService.js
@@ -2,8 +2,12 @@ const Admin = require('../models/Admin');
 
 module.exports = {
   findAll: async () => await Admin.findMany(),
+
   find: async (id) => await Admin.findOne(id),
+
   create: async (data) => await Admin.create(data),
+
   update: async (id, data) => await Admin.update(id, data),
+
   delete: async (id) => await Admin.delete(id),
 };
